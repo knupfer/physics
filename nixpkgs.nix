13b20e1dec0cd1ed6b35a590b52dc31197ad2275
knupfer <fknupfer@gmail.com>
2018-10-15 17:27:47 +0200

    update nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 import (
   (import <nixpkgs> {}).fetchgit {
     url    = https://github.com/NixOS/nixpkgs.git;
-    rev    = "0a7e258012b60cbe530a756f09a4f2516786d370";
-    sha256 = "1qcnxkqkw7bffyc17mqifcwjfqwbvn0vs0xgxnjvh9w0ssl2s036";
+    rev    = "29d0eec15faa315b45bbae08f43ac40e7cb2b4aa";
+    sha256 = "1if1c1hh1g1zqhfpwd4cgvdywbl2s3qv1bfzsngx9lmnh03n4v6j";
   }) {}
